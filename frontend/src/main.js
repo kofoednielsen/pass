8925c402d0f01e6bba3e010005467279d89e9206
Asbjørn Kofoed-Nielsen <asbjorn@kofoednielsen.com>
2023-08-07 19:06:38 +0200

    Postpone release of hell 🔥

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ const isDev = !location.host
 // First one here will be the fallback theme
 const knownThemes = ['snake', 'hell', 'sketch']
 const servers = [
-  `ws://${location.host}/hell`,
+  //  `ws://${location.host}/hell`,
   `ws://${location.host}/snake`,
   `ws://${location.host}/sketch`,
 ]
