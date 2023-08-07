c4828a497366e7f39cfdf4e932b714710dd57db3
Niels G. W. Serup <ngws@metanohi.name>
2023-08-07 12:04:42 +0200

    Clean up Haskell code

--- heaven/src/Server.hs
+++ heaven/src/Server.hs
@@ -2,12 +2,9 @@
 module Server (main) where
 
 import Data.Text (Text)
-import Control.Exception (finally)
-import Control.Monad (forM_, forever)
+import Control.Monad (forM_)
 import qualified Control.Concurrent as C
 import Control.Exception (handle)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
 import qualified Network.WebSockets as WS
 
@@ -22,29 +19,14 @@ port = 8080
 
 type Client = (Text, WS.Connection)
 
--- The state kept on the server is simply a list of connected clients. We've
--- added an alias and some utility functions, so it will be easier to extend
--- this state later on.
 type ServerState = [Client]
 
--- Create a new, initial state:
 newServerState :: ServerState
 newServerState = []
 
--- -- Get the number of active clients:
--- numClients :: ServerState -> Int
--- numClients = length
-
--- Check if a user already exists (based on username):
-clientExists :: Client -> ServerState -> Bool
-clientExists client = any ((== fst client) . fst)
-
--- Add a client (this does not check if the client already exists, you should do
--- this yourself using `clientExists`):
 addClient :: Client -> ServerState -> ServerState
 addClient client clients = client : clients
 
--- Remove a client:
 removeClient :: Client -> ServerState -> ServerState
 removeClient client = filter ((/= fst client) . fst)
 
@@ -72,7 +54,6 @@ receiveLoop conn state = do
   let request = Api.decodeText msg
   let client = (requestName request, conn)
   print request
-  clients <- C.readMVar state
   case requestAction request of
     Join -> do
       x <- C.modifyMVar_ state $ pure . addClient client
